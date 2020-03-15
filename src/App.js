15a19a9c5a136bb0006c818e59c26e40d726b3c4
Marina Massoud <korolem2@aalto.fi>
2020-03-15 11:36:03 +0200

    Create a cat fact app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import "./App.css"
 
-function App() {
+const App = () => {
+  const [facts, setFacts] = useState([])
+  const [fetching, setFetchingState] = useState(false)
+  const [error, setError] = useState({ error: false, message: "" })
+
+  const thingy = async () => {
+    try {
+        setFetchingState(true)
+        const a = await fetch("/facts/random?amount=5", {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+        if (a.status !== 200)
+          throw Error("Error in fetching data")
+        const b = await a.json()
+        setFacts(b)
+        console.log("SITUATION", b)
+        setError({ error: false, message: "" })
+        setFetchingState(false)
+    } catch (e) {
+      console.log(`${e}`)
+      setError({ error: true, message: e.message})
+      setFetchingState(false)
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Marina's cat facts</h1>
       </header>
+      <button onClick={thingy}>Reload facts</button>
+      <br></br>
+      {fetching ? (
+        <i className="fa fa-paw fa-spin" />
+      ) : error.error ? (
+        <div className="error">{error.message}</div>
+      ) : (
+        facts.map((fact, i) => {
+          return (
+            <div className="fact-box" key={i}>
+              <span className="index">{i+1}</span><br/>{fact.text}
+            </div>
+          )
+        })
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
